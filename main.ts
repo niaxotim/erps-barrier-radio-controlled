b5a9b10b30ee34e9169e5dab638c61f0aacca40e
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-25 11:11:15 +0000

    Added blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "up") {
+        Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
+    } else if (receivedString == "down") {
+        Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
+    }
 })
+radio.setGroup(255)
+Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
